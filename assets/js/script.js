e6615e0ec90fb755cd593f310c6c1e873e90a98b
tarahwaters <thbwaters@outlook.com>
2023-06-20 15:32:09 +0000

    Refactor code for disabling and unflipping cards depending on a pair match

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,21 +14,31 @@ function flipCard() {
     // second card click
     hasFlippedCard = false;
     secondCard = this;
+  }
+}
 
+function checkForMatch() {
     // do the two cards match?
     if (firstCard.dataset.match === secondCard.dataset.match) {
-      // it's a matching pair!
-      firstCard.removeEventListener('click', flipCard);
-      secondCard.removeEventListener('click', flipCard);
+    // it's a matching pair!
+      disableCards();
     } else {
-        // it's not a matching pair..
-        // delay the reset flip by adding a timeout
-        setTimeout(() => {
-          firstCard.classList.remove('flip');
-          secondCard.classList.remove('flip');
-        }, 2000);
+    // it's not a matching pair..
+    // delay the reset flip by adding a timeout
+       unflipCards(); 
     }
-  }
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 2000);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
