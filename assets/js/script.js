7eda7035c161c9dd5bc5695c7ccbec478e3230ac
tarahwaters <thbwaters@outlook.com>
2023-06-20 09:04:12 +0000

    Add js flipCard function to recognise first card click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 const cards = document.querySelectorAll('.game-card');
 
+let hasFlippedCard = false
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    console.log({hasFlippedCard, firstCard});
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
